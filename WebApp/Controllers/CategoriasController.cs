8ad21dd34ca750710505ddab22bcde7acf21d6c2
Emanuel Ferreira <emanuel2005batista@gmail.com>
2024-05-16 14:28:13 -0300

    Add project files.

---
+++ WebApp/Controllers/CategoriasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categorias = CategoriaRepository.GetCategorias();
+            return View(categorias);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Categoria categoria)
+        {
+            CategoriaRepository.AddCategoria(categoria);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var categoria = CategoriaRepository.GetCategoriaById(id);
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Categoria categoria)
+        {
+            CategoriaRepository.AtualizarCategoria(categoria.Id, categoria);
+            //return this.Index();
+            return RedirectToAction(nameof(Index));
+        }
+
+		public IActionResult Delete(int id)
+        {
+            CategoriaRepository.DeletarCategoria(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebApp/Models/CategoriaRepository.cs
@@ -0,0 +1,63 @@
+﻿namespace WebApp.Models
+{
+	public class CategoriaRepository
+	{
+		private static List<Categoria> categorias = new List<Categoria>()
+		{
+			new Categoria() { Id = 1, Nome = "Frutas", Descricao = "Seção de Frutas" },
+			new Categoria() { Id = 2, Nome = "Carnes", Descricao = "Seção de Carnes" }
+		};
+
+		public static void AddCategoria(Categoria categoria)
+		{
+			if(GetCategorias().Count() > 0)
+			{
+                var maxId = categorias.Max(c => c.Id);
+                categoria.Id = maxId + 1;
+                categorias.Add(categoria);
+            } else
+			{
+                categoria.Id = 1;
+                categorias.Add(categoria);
+            }
+			
+		}
+
+		public static List<Categoria> GetCategorias() => categorias;
+
+		public static Categoria? GetCategoriaById(int id)
+		{
+			var categoria = categorias.FirstOrDefault(x => x.Id == id);
+			if(categoria != null)
+			{
+				return new Categoria
+				{
+					Id = categoria.Id,
+					Nome = categoria.Nome,
+					Descricao = categoria.Descricao
+				};
+			}
+            return null;
+		}
+
+        public static void AtualizarCategoria(int id, Categoria categoria)
+        {
+            if (id != categoria.Id) return;
+            var categoriaAtualizada = categorias.FirstOrDefault(x => x.Id == id);
+            if (categoriaAtualizada != null)
+            {
+                categoriaAtualizada.Nome = categoria.Nome;
+                categoriaAtualizada.Descricao = categoria.Descricao;
+            }
+        }
+
+        public static void DeletarCategoria(int id)
+		{
+			var categoria = categorias.FirstOrDefault(x =>  id == x.Id);
+			if(categoria != null)
+			{
+				categorias.Remove(categoria);
+			}
+		}
+	}
+}
